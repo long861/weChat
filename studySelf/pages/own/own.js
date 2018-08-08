8755902e776bd50beaea38c5f8e86b14d0844e8e
马龙 <malong94@163.com>
2018-08-08 16:17:28 +0800

    this will display userInfo

--- studySelf/pages/own/own.js
+++ studySelf/pages/own/own.js
@@ -1,71 +1,86 @@
 // pages/own/own.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    isLink:true
-  
+    isLink: true,
+    userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    if(app.globalData.userInfo){
+      this.setData({
+        userInfo: app.globalData.userInfo
+      })
+    } else{
+      wx.getUserInfo({
+        success: res =>{
+          console.log('-=--=-=-=',res)
+          this.setData({
+            userInfo:res.userInfo
+          })
+        }
+      })
+    }
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   },
   onChange(event) {
-    console.log('====',event.detail)
+    console.log('====', event.detail)
     const detail = event.detail;
     this.setData({
       'isLink': detail.value
